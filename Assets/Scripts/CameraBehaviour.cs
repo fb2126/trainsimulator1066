bec0c885d2474d999d238f9cb8b8df36ec34c028
heemogoblin <artyom.boyarov@gmail.com>
2022-10-21 16:39:56 +0100

    Added 4 camera angles

--- Assets/Scripts/CameraBehaviour.cs
+++ Assets/Scripts/CameraBehaviour.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class CameraBehaviour : MonoBehaviour
 {
     public float speed = 5;
-    // Start is called before the first frame update
+    
+    public List<Vector3> cameraAngles = new List<Vector3>(5);
+    int prevCameraAngle = 0;
+    
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -19,7 +22,35 @@ public class CameraBehaviour : MonoBehaviour
             transform.eulerAngles += speed * new Vector3(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0);
             Debug.Log("X: " + Input.GetAxis("Mouse X"));
             Debug.Log("Y: " + Input.GetAxis("Mouse Y"));
-            
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            transform.position -= cameraAngles[prevCameraAngle];
+            transform.position += cameraAngles[0];
+            prevCameraAngle = 0;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            transform.position -= cameraAngles[prevCameraAngle];
+            transform.position += cameraAngles[1];
+            prevCameraAngle = 1;
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            transform.position -= cameraAngles[prevCameraAngle];
+            transform.position += cameraAngles[2];
+            prevCameraAngle = 2;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            transform.position -= cameraAngles[prevCameraAngle];
+            transform.position += cameraAngles[3];
+            prevCameraAngle = 3;
         }
     }
 }
